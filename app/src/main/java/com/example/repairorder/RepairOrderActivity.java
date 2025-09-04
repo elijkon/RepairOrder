4780faa9e0be403897fb3e5e94ebab4b544a8855
elijkon <timetocleanup20@gmail.com>
2025-09-04 12:30:36 -0500

    idk

--- app/src/main/java/com/example/repairorder/RepairOrderActivity.java
+++ app/src/main/java/com/example/repairorder/RepairOrderActivity.java
@@ -2,6 +2,9 @@ package com.example.repairorder;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,6 +17,35 @@ import java.util.Random;
 
 public class RepairOrderActivity extends AppCompatActivity {
     double numbers = 0.0;
+    TextView subtotalTV, totalTV;
+    Button submitB;
+    EditText partsET, laborET, paintET, inspectionET, orderET;
+
+    View.OnClickListener buttonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Random gen = new Random();
+
+            double number =  gen.nextDouble();
+            String amount  = "$ " + number;
+            subtotalTV.setText(amount);
+
+            String orderTypeVal = orderET.getText().toString();
+            Integer OT = Integer.getInteger(orderTypeVal);
+
+            String inspectionVal = inspectionET.getText().toString();
+            Double inspectValDouble = Double.parseDouble(inspectionVal);
+
+            String paintVal = paintET.getText().toString();
+            Double paintValDouble = Double.parseDouble(paintVal);
+
+            String partsVal = partsET.getText().toString();
+            Double partsValDouble = Double.parseDouble(partsVal);
+
+            String laborVal = laborET.getText().toString();
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,15 +62,14 @@ public class RepairOrderActivity extends AppCompatActivity {
         });
 
 
-        TextView totalTV = findViewById(R.id.totalAmount);
-
-        Random gen = new Random();
-        double number =  gen.nextDouble();
-        String amount  = "$ " + number;
-        totalTV.setText(amount);
-
-//        totalTV.setText(R.string.dollarempty);
-        String value = totalTV.getText().toString();
-        Log.i("Test", value);
+        totalTV = findViewById(R.id.totalAmount);
+        submitB = findViewById(R.id.submitButton);
+        submitB.setOnClickListener(buttonListener);
+        subtotalTV = findViewById(R.id.subtotalAmount);
+        orderET = findViewById(R.id.typeInput);
+        inspectionET = findViewById(R.id.inspectInput);
+        paintET = findViewById(R.id.paintInput);
+        partsET = findViewById(R.id.partsInput);
+        laborET = findViewById(R.id.laborInput);
     }
 }
