a967787b79cff9e43b465aa1a567cd069a4ea838
elijkon <timetocleanup20@gmail.com>
2025-09-12 14:31:26 -0500

    1. Create a new string array resource 2. Add a Spinner to the UI 3. Bind the Spinner to arrays resource in Java code 4. Create and bind an OnItemSelectedListener to the spinner

--- app/src/main/java/com/example/repairorder/RepairOrderActivity.java
+++ app/src/main/java/com/example/repairorder/RepairOrderActivity.java
@@ -3,8 +3,11 @@ package com.example.repairorder;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -19,7 +22,19 @@ public class RepairOrderActivity extends AppCompatActivity {
     TextView subtotalTV, totalTV, taxTV;
     Button submitB;
     EditText partsET, laborET, paintET, inspectionET, orderET;
+    Spinner ordSpinner;
 
+
+    AdapterView.OnItemSelectedListener spinnerListener = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            String selectedOrderType = parent.getItemAtPosition(position).toString();
+        }
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    };
     View.OnClickListener buttonListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -60,6 +75,7 @@ public class RepairOrderActivity extends AppCompatActivity {
         }
     };
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -82,10 +98,22 @@ public class RepairOrderActivity extends AppCompatActivity {
         submitB.setOnClickListener(buttonListener);
 
         subtotalTV = findViewById(R.id.subtotalAmount);
-        orderET = findViewById(R.id.typeInput);
+        //orderET = findViewById(R.id.typeInput);
         inspectionET = findViewById(R.id.inspectInput);
         paintET = findViewById(R.id.paintInput);
         partsET = findViewById(R.id.partsInput);
         laborET = findViewById(R.id.laborInput);
+
+        ordSpinner = findViewById(R.id.orderSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.order_types,
+                android.R.layout.simple_spinner_item
+        );
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ordSpinner.setAdapter(adapter);
+
+
     }
 }
